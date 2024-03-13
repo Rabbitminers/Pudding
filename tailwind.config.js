cbd0d10dc49bd25b408f8c5a8b5f62ae4e60333e
Rabbitminers <Rabbitminers2.0@gmail.com>
2024-03-13 20:18:47 +0000

    Added default tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        primary: '#570df8',
+        secondary: '#f000b8',
+        accent: '#1F1C2E',
+        neutral: '#FFFFFF',
+        'base-100': '#F3F6FD',
+        info: '#3abff8',
+        success: '#36d399',
+        warning: '#fbbd23',
+        error: '#f87272'
+      }
+    }
   },
   plugins: []
 };
