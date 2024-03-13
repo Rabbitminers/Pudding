3a9c3235f4b902754faf3bb76c240e244dc2343c
Rabbitminers <Rabbitminers2.0@gmail.com>
2024-03-13 20:23:25 +0000

    Implemented placeholder settings page

---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Layout from '$lib/components/Layout.svelte';
+	import Home from '$lib/components/icons/Home.svelte';
+</script>
+
+<Layout>
+	<svelte:fragment slot="title">Settings</svelte:fragment>
+
+	<svelte:fragment slot="actions">
+		<a href="/"><Home /></a>
+	</svelte:fragment>
+
+	<article class="grid grid-cols-2 gap-4">Settings!</article>
+</Layout>
