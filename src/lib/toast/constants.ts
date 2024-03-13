44199bb162367c81c6a09446b67f8f024aac9971
Rabbitminers <Rabbitminers2.0@gmail.com>
2024-03-13 20:22:35 +0000

    Added default toast configs

---
+++ src/lib/toast/constants.ts
@@ -0,0 +1,5 @@
+import type { ToastOptions } from 'svelte-french-toast'
+
+export const ToastConfig: ToastOptions = {
+    position: "bottom-center"
+}
---
+++ src/lib/toast/toaster.ts
@@ -0,0 +1,5 @@
+import toast from 'svelte-french-toast';
+
+function error() {
+    toast.error("Oh no!");
+}
