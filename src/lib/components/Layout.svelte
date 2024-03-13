230822adbd0af52ac8c7591111ebe762441b9586
Rabbitminers <Rabbitminers2.0@gmail.com>
2024-03-13 20:22:12 +0000

    Implemented generic material design type layout component

---
+++ src/lib/components/Layout.svelte
@@ -0,0 +1,13 @@
+<main class="flex flex-col h-full overflow-hidden">
+	<header class="p-6 pt-12 shadow-lg flex flex-row justify-between bg-neutral">
+		<h1 class="text-xl font-bold text-center"><slot name="title" /></h1>
+
+		<section class="flex flex-row justify-end gap-4">
+			<slot name="actions" />
+		</section>
+	</header>
+
+	<article id="body" class="flex-grow overflow-scroll p-4 flex flex-col gap-4">
+		<slot />
+	</article>
+</main>
