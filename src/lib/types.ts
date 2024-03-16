33faa7a3757f24b9d29c9443a9f6c887384e94bb
Rabbitminers <Rabbitminers2.0@gmail.com>
2024-03-16 23:53:59 +0000

    Added reader theme loading

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,32 @@
-export interface EpubBook {
-    title: string;
-    authors: string[];
-    progress: number;
-    image_url: string;
+export interface Library {
+    books: LibraryEntry[];
+}
+
+export type LibraryEntry = {
+    book_id: string,
+    file: string,
+    lastRead: Date;
+    location: string | null;
+    completion: number;
+    title: string | null;
+    coverImage: string | null;
+    author: string | null;
+}
+
+export interface ReaderTheme {
+    body: {
+      background?: string,
+      color?: string,
+    },
+    '*'?: {
+      color?: string,
+      background?: string,
+    },
+    'a:link'?: {
+      color?: string,
+      'text-decoration'?: string,
+    },
+    'a:link:hover'?: {
+      background?: string,
+    }
 }
