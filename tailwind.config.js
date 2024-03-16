45200da870dd0f67f9e3b85aa816dc63aed89f2e
Rabbitminers <Rabbitminers2.0@gmail.com>
2024-03-16 23:54:13 +0000

    Implemented theming

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,17 +4,11 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#570df8',
-        secondary: '#f000b8',
-        accent: '#1F1C2E',
-        neutral: '#FFFFFF',
-        'base-100': '#F3F6FD',
-        info: '#3abff8',
-        success: '#36d399',
-        warning: '#fbbd23',
-        error: '#f87272'
-      }
-    }
+        'accent': 'var(--colour-accent)',
+        'neutral': 'var(--colour-neutral)',
+        'base': 'var(--colour-base)',
+      },
+    },
   },
-  plugins: []
+  darkMode: 'class',
 };
