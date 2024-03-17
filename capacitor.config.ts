13f3b5d0273ae701cc2b348302307148272d3922
Rabbitminers <Rabbitminers2.0@gmail.com>
2024-03-17 16:11:54 +0000

    Removed server url from capacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,12 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.rabbitminers.pudding',
-  appName: 'Pudding',
-  webDir: 'build',
-  server: {
-    androidScheme: 'https',
-    url: "http://192.168.1.101:5173/",
-    cleartext: true
-  }
+	appId: 'com.rabbitminers.pudding',
+	appName: 'Pudding',
+	webDir: 'build',
+	server: {
+		androidScheme: 'https'
+	}
 };
 
 export default config;
